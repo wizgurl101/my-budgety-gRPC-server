4414441ee1098be7636fd9fe6089f5c0e4c0de94
wizgurl101 <nb.phan@hotmail.com>
2024-12-15 18:48:06 -0700

    implement get first date of current month

--- tests/date_helper_test.go
+++ tests/date_helper_test.go
@@ -24,3 +24,22 @@ func TestGetFirstDateOfNextMonth(t *testing.T) {
 		}
 	})
 }
+
+func TestGetFirstDateOfCurrentMonth(t *testing.T) {
+	t.Run("Given the date 2024-12-15 Then the date of the first of the month is returned", func(t *testing.T) {
+		original_get_current_date := utils.GetCurrentDate
+
+		// Restore the original GetCurrentDate function after the test
+		defer func() { utils.GetCurrentDate = original_get_current_date }()
+
+		utils.GetCurrentDate = func() time.Time {
+			return time.Date(2024, 12, 15, 0, 0, 0, 0, time.UTC)
+		}
+
+		expected := time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC).Format("2006-01-02")
+		actual := utils.GetFirstDateOfCurrentMonth()
+		if actual != expected {
+			t.Errorf("expected %s but got %s", expected, actual)
+		}
+	})
+}
--- utils/date_helper.go
+++ utils/date_helper.go
@@ -6,6 +6,12 @@ var GetCurrentDate = func() time.Time {
 	return time.Now()
 }
 
+func GetFirstDateOfCurrentMonth() string {
+	currentDate := GetCurrentDate()
+	currentMonthFirstDate := time.Date(currentDate.Year(), currentDate.Month(), 1, 0, 0, 0, 0, time.UTC)
+	return currentMonthFirstDate.Format("2006-01-02")
+}
+
 func GetFirstDateOfNextMonth() string {
 	currentDate := GetCurrentDate()
 	nextMonthFirstDate := time.Date(currentDate.Year(), currentDate.Month()+1, 1, 0, 0, 0, 0, time.UTC)
