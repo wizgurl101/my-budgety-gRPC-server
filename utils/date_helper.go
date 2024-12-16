6d9dd01447144d57231b8f0336275440d0941851
wizgurl101 <nb.phan@hotmail.com>
2024-12-15 18:46:34 -0700

    implement get the first date of next month based on current date

--- utils/date_helper.go
+++ utils/date_helper.go
@@ -6,9 +6,8 @@ var GetCurrentDate = func() time.Time {
 	return time.Now()
 }
 
-func GetLastDateOfCurrentMonth() string {
+func GetFirstDateOfNextMonth() string {
 	currentDate := GetCurrentDate()
 	nextMonthFirstDate := time.Date(currentDate.Year(), currentDate.Month()+1, 1, 0, 0, 0, 0, time.UTC)
-	lastDateOfCurrentMonth := nextMonthFirstDate.AddDate(0, -1, -1)
-	return lastDateOfCurrentMonth.Format("2006-01-02")
+	return nextMonthFirstDate.Format("2006-01-02")
 }
